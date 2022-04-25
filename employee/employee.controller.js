3ff9fd0eefe3762d43ca71d6f57130424a0d5d77
ayushkayshab04 <ayushkayshab04@gmail.com>
2022-04-25 19:34:54 +0530

    Second commit

--- employee/employee.controller.js
+++ employee/employee.controller.js
@@ -1,10 +1,12 @@
 const employeeService = require("./employee.service");
-
+//const successHandler = require("../common/handlers/sucess.handler")
 
 const getEmployeeList = async (req, res) => {
   try {
     const employeeList = await employeeService.getEmployeeList;
-    return successHandler({ employeeList }, req, res);
+    // return successHandler({ employeeList }, req, res);
+    console.log(employeeList)
+    res.send("Employee list Found")
   } catch (err) {
     throw new Error(err);
   }
